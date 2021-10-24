88ad66de542905aee4a5762baea42d2cd621d8ae
webmaster978 <jtjoel11@gmail.com>
2021-10-25 00:25:36 +0200

    partie admin

--- about.php
+++ about.php
@@ -7,18 +7,15 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../../../stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/A.animate.css%2bowl.carousel.min.css%2bowl.theme.default.min.css%2bmagnific-popup.css%2bflaticon.css%2bstyle.css%2cMcc.tEAbUIjGTj.css.pagespeed.cf.swv2" />
+<link rel="stylesheet" href="font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="wrap">
 <div class="container">
 <div class="row">
 <div class="col-md-6 d-flex align-items-center">
-<p class="mb-0 phone pl-md-2">
-<a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> +00 1234 567</a>
-<a href="#"><span class="fa fa-paper-plane mr-1"></span> <span class="__cf_email__" data-cfemail="dba2b4aea9beb6bab2b79bbeb6bab2b7f5b8b4b6">[email&#160;protected]</span></a>
-</p>
+<?php include'part/_phone.php'; ?>
 </div>
 <div class="col-md-6 d-flex justify-content-md-end">
 <div class="social-media">
@@ -35,31 +32,31 @@
 </div>
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 <div class="container">
-<a class="navbar-brand" href="index.html">Counselor</a>
+<a class="navbar-brand" href="akiba">Akiba_manur</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 <span class="oi oi-menu"></span> Menu
 </button>
 <div class="collapse navbar-collapse" id="ftco-nav">
 <ul class="navbar-nav ml-auto">
-<li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-<li class="nav-item active"><a href="about.html" class="nav-link">About</a></li>
-<li class="nav-item"><a href="counselor.html" class="nav-link">Counselor</a></li>
-<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+<li class="nav-item"><a href="akiba" class="nav-link">Home</a></li>
+<li class="nav-item active"><a href="about" class="nav-link">A propos</a></li>
+<li class="nav-item"><a href="team" class="nav-link">Equipe</a></li>
+<li class="nav-item"><a href="service" class="nav-link">Services</a></li>
+
+<li class="nav-item"><a href="blog" class="nav-link">Blog</a></li>
+<li class="nav-item"><a href="contact" class="nav-link">Contact</a></li>
 </ul>
 </div>
 </div>
 </nav>
 
-<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_5.jpg');" data-stellar-background-ratio="0.5">
+<section class="hero-wrap hero-wrap-2" style="background-image: url('img/12.jpg');" data-stellar-background-ratio="0.5">
 <div class="overlay"></div>
 <div class="container">
 <div class="row no-gutters slider-text align-items-end justify-content-center">
 <div class="col-md-9 ftco-animate mb-5 text-center">
-<p class="breadcrumbs mb-0"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>About us <i class="fa fa-chevron-right"></i></span></p>
-<h1 class="mb-0 bread">About Us</h1>
+<p class="breadcrumbs mb-0"><span class="mr-2"><a href="akiba">Home <i class="fa fa-chevron-right"></i></a></span> <span>About us <i class="fa fa-chevron-right"></i></span></p>
+<h1 class="mb-0 bread">A propos de nous</h1>
 </div>
 </div>
 </div>
@@ -215,12 +212,13 @@
 </div>
 </div>
 </section>
-<section class="ftco-appointment ftco-section img" style="background-image: url(images/bg_2.jpg);">
+
+<section class="ftco-appointment ftco-section img" style="background-image: url(img/6.jpg);">
 <div class="overlay"></div>
 <div class="container">
 <div class="row">
 <div class="col-md-6 half ftco-animate">
-<h2 class="mb-4">Send a Message &amp; Get in touch!</h2>
+<h2 class="mb-4">Envoyer nous un message</h2>
 <form action="#" class="appointment">
 <div class="row">
 <div class="col-md-6">
@@ -237,16 +235,8 @@
 <div class="form-group">
 <div class="form-field">
 <div class="select-wrap">
-<div class="icon"><span class="fa fa-chevron-down"></span></div>
-<select name="" id="" class="form-control">
-<option value="">Services</option>
-<option value="">Relation Problem</option>
-<option value="">Couple Counseling</option>
-<option value="">Depression Treatment</option>
-<option value="">Family Problem</option>
-<option value="">Personal Problem</option>
-<option value="">Business Problem</option>
-</select>
+
+<input type="text" class="form-control" placeholder="Email">
 </div>
 </div>
 </div>
@@ -258,7 +248,7 @@
 </div>
 <div class="col-md-12">
 <div class="form-group">
-<input type="submit" value="Send message" class="btn btn-primary py-3 px-4">
+<input type="submit" value="Envoyer le message" class="btn btn-primary py-3 px-4">
 </div>
 </div>
 </div>
@@ -267,79 +257,8 @@
 </div>
 </div>
 </section>
-<footer class="ftco-footer">
-<div class="container">
-<div class="row mb-5">
-<div class="col-sm-12 col-md">
-<div class="ftco-footer-widget mb-4">
-<h2 class="ftco-heading-2 logo"><a href="#">Counselor</a></h2>
-<p>Far far away, behind the word mountains, far from the countries.</p>
-<ul class="ftco-footer-social list-unstyled mt-2">
-<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-<li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-<li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
-</ul>
-</div>
-</div>
-<div class="col-sm-12 col-md">
-<div class="ftco-footer-widget mb-4 ml-md-4">
-<h2 class="ftco-heading-2">Explore</h2>
-<ul class="list-unstyled">
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>What We Do</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Plans &amp; Pricing</a></li>
-</ul>
-</div>
-</div>
-<div class="col-sm-12 col-md">
-<div class="ftco-footer-widget mb-4 ml-md-4">
-<h2 class="ftco-heading-2">Legal</h2>
-<ul class="list-unstyled">
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Join us</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Privacy &amp; Policy</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Term &amp; Conditions</a></li>
-</ul>
-</div>
-</div>
-<div class="col-sm-12 col-md">
-<div class="ftco-footer-widget mb-4">
-<h2 class="ftco-heading-2">Company</h2>
-<ul class="list-unstyled">
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
-<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
-</ul>
-</div>
-</div>
-<div class="col-sm-12 col-md">
-<div class="ftco-footer-widget mb-4">
-<h2 class="ftco-heading-2">Have a Questions?</h2>
-<div class="block-23 mb-3">
-<ul>
-<li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-<li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text"><span class="__cf_email__" data-cfemail="d2bbbcb4bd92abbda7a0b6bdbfb3bbbcfcb1bdbf">[email&#160;protected]</span></span></a></li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-</div>
-<div class="container-fluid px-0 py-5 bg-black">
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-<p class="mb-0" style="color: rgba(255,255,255,.5);">
-Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com/" target="_blank">Colorlib.com</a>
-</p>
-</div>
-</div>
-</div>
-</div>
-</footer>
+
+<?php include'part/_foot.php'; ?>
 
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" /><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
 <script src="js/jquery.min.js"></script>
